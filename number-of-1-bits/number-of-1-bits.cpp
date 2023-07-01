7f5ad3f491435a76c354cd2783f69e95f0574359
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2023-07-01 11:15:50 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (27.13%) - LeetSync

--- number-of-1-bits/number-of-1-bits.cpp
+++ number-of-1-bits/number-of-1-bits.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        return __builtin_popcountll(n);
+        int cnt = 0;
+        while(n) {
+            if(n&1) {
+                cnt++;
+            }
+            n >>= 1;
+        }
+        return cnt;
     }
 };
