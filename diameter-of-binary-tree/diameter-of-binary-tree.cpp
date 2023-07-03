0ebc6b5c371268a2f8b4764b21c0233c49877d90
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2023-07-04 01:22:34 +0530

    Time: 42 ms (5.32%) | Memory: 20.2 MB (80.73%) - LeetSync

---
+++ diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -0,0 +1,38 @@
+/*
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int maxDepth(TreeNode* root) {
+        if(!root) {
+            return 0;
+        }
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
+    }
+    int DiaCalc(TreeNode* root, int &res) {
+        if(!root) {
+            return 0;
+        }
+        res = max(res,maxDepth(root->left)+maxDepth(root->right));
+        DiaCalc(root->left, res);
+        DiaCalc(root->right, res);
+        return res;
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans = -1;
+        if(!root) {
+            return ans+1;
+        }
+        DiaCalc(root,ans);
+        return ans;
+    }
+};
