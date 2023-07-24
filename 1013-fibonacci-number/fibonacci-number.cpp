d74e1f7bb2a61163d1a08ec99fa12fe1152f7ac8
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2023-07-25 01:48:54 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (24.23%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -8,11 +8,15 @@ private:
         if(f[n] != -1) {
             return f[n];
         }
-        return fib1(n-1,f) + fib1(n-2,f);
+        return f[n] = fib1(n-1,f) + fib1(n-2,f);
     }
 public:
     int fib(int n) {
         vector<int> f(31,-1);
+        fib1(n,f);
+        for(int i=0; i<31; i++) {
+            cout << f[i] << " ";
+        }
         return fib1(n,f);
     }
 };
