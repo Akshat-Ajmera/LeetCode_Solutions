8d742b550b559ec3a392d76bf0e7f607260b3d79
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2023-11-27 22:59:06 +0530

    Time: 70 ms (55.95%) | Memory: 59.2 MB (88.35%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
-    int maxArea(vector<int>& h) {
-        int n = h.size();
-        long long ans = min(h[n-1],h[0])*(n-1), i=0, j=n-1;
-        while(i<j && i>=0 && j<n) {
-            if(h[j] <= h[i]) {
-                j--;
-            }
-            else {
+    int maxArea(vector<int> &h) {
+        int n = h.size(), i = 0, j = n-1, area = ((j-i)*min(h[i],h[j]));
+        while(i<j) {
+            if(h[i] < h[j]) {
                 i++;
             }
-            ans = max(ans, min(h[j],h[i])*(j-i));
+            else {
+                j--;
+            }
+            area = max(area,((j-i)*min(h[i],h[j])));
         }
-        return ans;
+        return area;
     }
 };
+
+
+/*
+            if(h[i] < h[j]) {
+                i++;
+            }
+            else {
+                j--;
+            }
+
+    we move the ptr with the min height because we know we cant get a better ans than this
+    if we move the ptr with the longer height, than we have 3 cases
+    the next height is longer, shorter or equal to the other unmoved height
+    in which case we will take the unmoved in the 1st case, shorter in the second and both in the third, in all three cases our answers dont improve 
+*/
