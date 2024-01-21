afe0da9ea23146c597207a71a9ceffcdb53b4d23
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2024-01-22 04:52:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (5.08%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,37 +1,23 @@
 class Solution {
-// private:
-//     int solve(vector<int> &nums, vector<int> &dp, int n) {
-//         if(n<0) {
-//             return 0;
-//         }
-//         if(dp[n] != INT_MIN) {
-//             return dp[n];
-//         }
-//         if(!n) {
-//             return dp[n] = nums[n];
-//         }
-//         int p = nums[n] + solve(nums,dp,n-2);
-//         int np = solve(nums,dp,n-1);
-//         return dp[n] = max(p,np);
-//     }
+private:
+    int solve(int n, vector<vector<int>>& dp, int prev, vector<int> &v) {
+        if(n >= v.size()) {
+            return 0;
+        }
+        if(dp[n][prev+1] != -1) {
+            return dp[n][prev+1];
+        }
+        int notTake = solve(n+1,dp,prev,v), take = 0;
+        if(prev == -1 || prev != n-1) {
+            take = v[n] + solve(n+1,dp,n,v);
+        }
+        return dp[n][prev+1] = max(take,notTake);
+    }
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size(), p=0, np=0, slast=0, last=0, ans=0;
-        if(n==1) {
-            return nums[0];
-        }
-        // vector<int> dp(n,-1);
-        // return solve(nums,dp,n-1);
-        slast = nums[0];
-        last = max(nums[0],nums[1]);
-        ans = max(slast,last);
-        for(int i=2; i<n; i++) {
-            p = nums[i] + slast;
-            np = last;
-            ans = max(p,np);
-            slast = last;
-            last = ans;
-        }
-        return ans;
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int> (n, -1));
+        return solve(0,dp,-1,nums);
+        return 0;
     }
 };
