2e4f8fde1576b92c50c03aedc8bf2d582fd324de
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2024-02-12 02:05:49 +0530

    Time: 104 ms (53.33%) | Memory: 62.6 MB (74.90%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -12,23 +12,15 @@
 class Solution {
 private:
     void inorderTraversal(TreeNode* root, vector<int> &in) {
-        if(root->left) {
-            inorderTraversal(root->left, in);
-        }
+        if(root->left) inorderTraversal(root->left, in);
         in.push_back(root->val);
-        if(root->right) {
-            inorderTraversal(root->right, in);
-        }
+        if(root->right) inorderTraversal(root->right, in);
     }
     TreeNode* buildBST(vector<int> &in, int lo, int hi) {
         int mid = (lo+hi)/2;
         TreeNode* root = new TreeNode(in[mid]);
-        if(mid-1 >= lo) {
-            root->left = buildBST(in, lo, mid-1);
-        }
-        if(mid+1 <= hi) {
-            root->right = buildBST(in, mid+1, hi);
-        }
+        if(mid-1 >= lo) root->left = buildBST(in, lo, mid-1);
+        if(mid+1 <= hi) root->right = buildBST(in, mid+1, hi);
         return root;
     }
 public:
