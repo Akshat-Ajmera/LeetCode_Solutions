655c313ca2ac5b834e13fb72111f27b51636d18a
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2024-04-03 19:39:49 +0530

    Time: 1564 ms (7.27%) | Memory: 536.8 MB (7.27%) - LeetSync

---
+++ 464-can-i-win/can-i-win.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    bool calc(int bitmask, int player, int mxm, int tot, vector<vector<int>> &dp) {
+        if(tot <= 0) return false;
+        if(dp[bitmask][player] != -1) return dp[bitmask][player];
+        for(int i=0; i<mxm; i++) {
+            if((1 << i) & bitmask) continue;
+            if(tot <= i+1 || !calc(bitmask | (1 << i), player^1, mxm, tot-i-1, dp)) {
+                return dp[bitmask][player] = true;
+            }
+        }
+        return dp[bitmask][player] = false;
+    }
+public:
+    bool canIWin(int mxm, int tot) {
+        if(!tot || mxm >= tot) return true;
+        if((mxm*(mxm+1))/2 < tot) return false;
+        vector<vector<int>> dp(1 << (mxm), vector<int> (2, -1));
+        return calc(0, 0, mxm, tot, dp);
+    }
+};
