6397a327caa94de9923e2c24c332a5936fdf242d
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2024-04-19 16:17:41 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (69.02%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size(), m = nums2.size();
-        vector<int> ans(n);
-        stack<int> st;
-        unordered_map<int,int> mp;
-        mp.insert({nums2[m-1],-1});
-        st.push(nums2[m-1]);
-        for(int i=m-2; i>=0; i--) {
-            while(!st.empty() && nums2[i] >= st.top()) {
-                st.pop();
-            }
-            if(st.empty()) {
-                mp.insert({nums2[i],-1});
-            }
-            else {
-                mp.insert({nums2[i],st.top()});
-            }
-            st.push(nums2[i]);
+        stack<int> stk;
+        int n = nums2.size(), n1 = nums1.size();
+        vector<int> ans(n1);
+        map<int, int> mp;
+        for(int i=n-1; i>=0; i--) {
+            while(!stk.empty() && (stk.top() <= nums2[i])) stk.pop();
+            if(stk.empty()) mp[nums2[i]] = -1;
+            else mp[nums2[i]] = stk.top();
+            stk.push(nums2[i]);
         }
-        for(int i=0; i<n; i++) {
-            ans[i] = mp[nums1[i]];
-        }
+        for(int i=0; i<n1; i++) ans[i] = mp[nums1[i]];
         return ans;
     }
 };
