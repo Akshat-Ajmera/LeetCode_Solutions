89c8cb16577b0fc87fd864404fb242e9cb18a946
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2024-06-20 03:24:36 +0530

    Time: 43 ms (66.11%) | Memory: 51 MB (23.37%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,27 +1,18 @@
 class Solution {
-    bool solve(vector<int> &v, int n, vector<int> &dp, int siz) {
-        if(n == v.size()-1) {
-            return true;
-        }
-        if(!v[n]) {
-            return false;
-        }
-        if(dp[n] != -1) {
-            return dp[n];
-        }
-        bool ans = false;
-        for(int i=1; n+i<siz && i<=v[n]; i++) {
-            ans |= solve(v,n+i,dp,siz);
-            if(ans) {
-                return ans;
-            }
-        }
-        return dp[n] = ans;
-    }
 public:
     bool canJump(vector<int>& nums) {
-        int siz = nums.size();
-        vector<int> dp(siz,-1);
-        return solve(nums,0,dp,siz);
+        int n = nums.size(), canreach = 0;
+        for(int i=0; i<n; i++) {
+            if(i <= canreach) {
+                canreach = max(canreach, nums[i] + i);
+                if(canreach >= n-1) {
+                    return true;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        return false;
     }
 };
