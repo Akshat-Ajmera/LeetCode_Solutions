1f7219d9b04394e923cdf0040108348ac4639f38
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2024-06-22 00:51:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (19.20%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,29 +1,25 @@
 class Solution {
 private:
-    int solve(string s1, string s2, vector<vector<int>> &dp, int n, int m) {
-        if(n<0) {
-            return m+1;
-        }
-        if(m<0) {
-            return n+1;
-        }
-        if(dp[n][m] != -1) {
-            return dp[n][m];
-        }
-        if(s1[n]==s2[m]) {
-            return dp[n][m] = solve(s1,s2,dp,n-1,m-1);
+    int solve(int m, int n, string &s1, string &s2, vector<vector<int>> &dp) {
+        if(m < 0) return n+1;
+        if(n < 0) return m+1;
+        if(dp[m][n] != -1) return dp[m][n];
+        int ans = 0, a = 0, b = 0, c = 0;
+        if(s1[m] == s2[n]) {
+            ans = solve(m-1, n-1, s1, s2, dp);
         }
         else {
-            int a = 1 + solve(s1,s2,dp,n,m-1);
-            int b = 1 + solve(s1,s2,dp,n-1,m);
-            int c = 1 + solve(s1,s2,dp,n-1,m-1);
-            return dp[n][m] = min({a,b,c});
+            a = solve(m-1, n, s1, s2, dp);
+            b = solve(m, n-1, s1, s2, dp);
+            c = solve(m-1, n-1, s1, s2, dp);
+            ans = 1 + min({a, b, c});
         }
+        return dp[m][n] = ans;
     }
 public:
     int minDistance(string word1, string word2) {
-        int n = word1.size(), m = word2.size();
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        return solve(word1,word2,dp,n-1,m-1);
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return solve(m-1, n-1, word1, word2, dp);
     }
 };
