5c9ef1c0cc492433283e1c8d5c02ab0ada4a72ee
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2024-06-23 19:04:36 +0530

    Time: 2 ms (62.29%) | Memory: 11.1 MB (11.78%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    void solve(vector<int> nums, vector<int> v, vector<vector<int>> &ans) {
+        if(v.size() == nums.size()) {
+            ans.push_back(v);
+            return;
+        }
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i] == INT_MIN) continue;
+            v.push_back(nums[i]);
+            int temp = nums[i];
+            nums[i] = INT_MIN;
+            solve(nums, v, ans);
+            nums[i] = temp;
+            v.pop_back();
+        }
+        return;
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> v;
+        vector<vector<int>> ans;
+        solve(nums, v, ans);
+        return ans;
+    }
+};
