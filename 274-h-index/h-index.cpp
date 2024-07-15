39b6eedd13efcf2a8ad8e1bccbd20cbf8fdf7590
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2024-07-15 20:42:15 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (15.30%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hIndex(vector<int>& cit) {
+        int n = cit.size(), lo = 0, hi = n-1, mid;
+        sort(cit.begin(), cit.end());
+        while(lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if(cit[mid] < n - mid) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return n - lo;
+    }
+};
