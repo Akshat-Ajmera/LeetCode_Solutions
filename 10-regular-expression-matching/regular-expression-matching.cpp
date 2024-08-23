b0c40aa17c1183cfa343abe349dac6bf055cead0
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2024-08-24 03:53:30 +0530

    Time: 17 ms (14.33%) | Memory: 7.9 MB (84.92%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    bool solve(string &str, string &ptn, int n, int m) {
+        if(n < 0 && m < 0) return true;
+        if(n < 0) {
+            if(ptn[m] != '*') return false;
+            else return solve(str, ptn, n, m-2);
+        }
+        if(m < 0) return false;
+        bool ans = false;
+        if(ptn[m] == '.' || ptn[m] == str[n]) ans = solve(str, ptn, n-1, m-1);
+        else if(ptn[m] == '*') {
+            if(ptn[m-1] == '.' || ptn[m-1] == str[n]) ans = solve(str, ptn, n-1, m) || solve(str, ptn, n-1, m-2) || solve(str, ptn, n, m-2);
+            else ans = solve(str, ptn, n, m-2);
+        }
+        return ans;
+    }
+public:
+    bool isMatch(string str, string ptn) {
+        int n = str.size(), m = ptn.size();
+        return solve(str, ptn, n-1, m-1);
+    }
+};
