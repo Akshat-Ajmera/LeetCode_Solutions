743fbcaa5001ddc561121eff231a99cd90641256
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2025-04-02 17:42:41 +0530

    Time: 4 ms (16.57%) | Memory: 22.6 MB (12.05%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -8,9 +8,7 @@ public:
         suf[n-1] = nums[n-1];
         for(int i=1; i<n; i++) pre[i] = max(pre[i-1], nums[i-1]);
         for(int i=n-2; i>=0; i--) suf[i] = max(suf[i+1], nums[i+1]);
-        for(int j=1; j<n-1; j++) {
-            ans = max(ans, (long long)(pre[j] - nums[j]) * suf[j]);
-        }
+        for(int j=1; j<n-1; j++) ans = max(ans, (long long)(pre[j] - nums[j]) * suf[j]);
         return ans;
     }
 };
