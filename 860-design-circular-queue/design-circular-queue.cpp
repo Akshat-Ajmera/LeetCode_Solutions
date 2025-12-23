2d215fc2a143c6446e04ffb5faffe846a718d423
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2025-12-23 15:32:56 +0530

    Time: 5 ms (28.65%) | Memory: 23.4 MB (94.17%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.cpp
+++ 860-design-circular-queue/design-circular-queue.cpp
@@ -20,31 +20,8 @@ public:
         begin = (begin + 1) % k;
         return true;
     }
-    
-    int Front() {
-        return isEmpty() ? -1 : circularQueue[begin];
-    }
-    
-    int Rear() {
-        return isEmpty() ? -1 : circularQueue[(end - 1 + k) % k];
-    }
-    
-    bool isEmpty() {
-        return !cnt;
-    }
-    
-    bool isFull() {
-        return cnt == k;
-    }
+    int Front() { return isEmpty() ? -1 : circularQueue[begin]; }
+    int Rear() { return isEmpty() ? -1 : circularQueue[(end - 1 + k) % k]; }
+    bool isEmpty() { return !cnt; }
+    bool isFull() { return cnt == k; }
 };
-
-/**
- * Your MyCircularQueue object will be instantiated and called as such:
- * MyCircularQueue* obj = new MyCircularQueue(k);
- * bool param_1 = obj->enQueue(value);
- * bool param_2 = obj->deQueue();
- * int param_3 = obj->Front();
- * int param_4 = obj->Rear();
- * bool param_5 = obj->isEmpty();
- * bool param_6 = obj->isFull();
- */
