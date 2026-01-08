36e5263c6f02e0afe49cf432159daa3c0407596c
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2026-01-08 20:14:01 +0530

    Time: 42 ms (24.40%) | Memory: 18.9 MB (25.20%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -1,16 +1,18 @@
 class Solution {
 private:
     int solve(int i, int j, vector<int> &u, vector<int> &v, vector<vector<int>> &dp) {
-        if(i < 0 || j < 0) return 0;
+        if(i < 0 || j < 0) return -50000000;
         if(dp[i][j] != -1) return dp[i][j];
-        int a = (u[i] * v[j]) + solve(i-1, j-1, u, v, dp);
+        int a = solve(i-1, j-1, u, v, dp);
         int b = solve(i-1, j, u, v, dp);
         int c = solve(i, j-1, u, v, dp);
-        return dp[i][j] = max({a, b, c});
+        int d = (u[i] * v[j]);
+        return dp[i][j] = max({a + d, b, c, d});
     }
 public:
     int maxDotProduct(vector<int>& u, vector<int>& v) {
         vector<vector<int>> dp(u.size(), vector<int> (v.size(), -1));
+        return solve(u.size()-1, v.size()-1, u, v, dp);
         int ans = solve(u.size()-1, v.size()-1, u, v, dp), zero = 0, mnm_u =INT_MAX, mnm_v = INT_MAX, mxm_u = INT_MIN, mxm_v = INT_MIN;
         if(ans) return ans;
         for(int &it : u) {
