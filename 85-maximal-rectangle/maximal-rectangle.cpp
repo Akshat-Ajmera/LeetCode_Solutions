95c9d9c5c8a178556e3398a8ddb3af6e20811007
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2026-01-11 18:24:02 +0530

    Time: 5 ms (66.95%) | Memory: 19.6 MB (37.84%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,35 +1,39 @@
 class Solution {
 private:
-    void solve(vector<int> &h, vector<int> &nsi, vector<int> &psi) {
-        int n = h.size();
+    int solve(vector<int> &v) {
+        int n = v.size(), ans = 0;
+        vector<int> nse(n, n), pse(n, -1);
         stack<int> stk;
-        for(int i=0; i<n; i++) {
-            while(!stk.empty() && h[stk.top()] >= h[i]) stk.pop();
-            if(stk.empty()) psi[i] = -1;
-            else psi[i] = stk.top();
+        for(int i=n-1; i>=0; i--) {
+            if(!stk.empty()) {
+                while(!stk.empty() && v[stk.top()] >= v[i]) stk.pop();
+                if(!stk.empty()) nse[i] = stk.top();
+            }
             stk.push(i);
         }
         while(!stk.empty()) stk.pop();
-        for(int i=n-1; i>=0; i--) {
-            while(!stk.empty() && h[stk.top()] >= h[i]) stk.pop();
-            if(stk.empty()) nsi[i] = n;
-            else nsi[i] = stk.top();
+        for(int i=0; i<n; i++) {
+            if(!stk.empty()) {
+                while(!stk.empty() && v[stk.top()] >= v[i]) stk.pop();
+                if(!stk.empty()) pse[i] = stk.top();
+            }
             stk.push(i);
         }
+        for(int i=0; i<n; i++) {
+            ans = max(ans, v[i] * (nse[i] - pse[i] - 1));
+        }
+        return ans;
     }
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int ans = 0, m = matrix.size(), n = matrix[0].size();
-        vector<int> height(n, 0), nsi(n), psi(n);
+        int m = matrix.size(), n = matrix[0].size(), ans = 0;
+        vector<int> v(n);
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                if(matrix[i][j] == '0') height[j] = 0;
-                else height[j]++;
+                if(!(matrix[i][j] - '0')) v[j] = 0;
+                else v[j]++;
             }
-            solve(height, nsi, psi);
-            for(int j=0; j<n; j++) {
-                ans = max(ans, height[j] * (nsi[j] - psi[j] - 1));
-            }
+            ans = max(ans, solve(v));
         }
         return ans;
     }
