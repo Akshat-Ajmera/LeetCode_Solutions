e27c866e9163f40bfe5bf1aec7cc4c118119cc88
Akshat Ajmera <106649952+Akshat-Ajmera@users.noreply.github.com>
2026-05-01 00:51:49 +0530

    Time: 0 ms (100.00%) | Memory: 26.9 MB (80.97%) - LeetSync

--- 1767-design-front-middle-back-queue/design-front-middle-back-queue.cpp
+++ 1767-design-front-middle-back-queue/design-front-middle-back-queue.cpp
@@ -1,71 +1,120 @@
+struct LLNode {
+    int val;
+    LLNode* prev;
+    LLNode* next;
+
+    LLNode(int data) {
+        val = data;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
 class FrontMiddleBackQueue {
 private:
-    deque<int> front, back;
-    int sizeDiff() { return (front.size() - back.size()); }
+    int cnt = 0;
+    LLNode* front = NULL;
+    LLNode* middle = NULL;
+    LLNode* back = NULL;
 public:
     FrontMiddleBackQueue() {}
     void pushFront(int val) {
-        if(sizeDiff()) {
-            int last = front.back();
-            back.push_front(last);
-            front.pop_back();
+        LLNode* node = new LLNode(val);
+        node -> next = front;
+        if(front) front -> prev = node;
+        front = node;
+        if(!cnt) {
+            back = front;
+            middle = front;
         }
-        front.push_front(val);
+        else if(cnt % 2) middle = middle -> prev;
+        cnt++;
         return;
     }
     void pushMiddle(int val) {
-        if(sizeDiff()) {
-            int last = front.back();
-            back.push_front(last);
-            front.pop_back();
+        LLNode* node = new LLNode(val);
+        if(!cnt) {
+            front = back = middle = node;
+            cnt++;
+            return;
         }
-        front.push_back(val);
+        if(cnt == 1) {
+            node -> next = front;
+            front -> prev = node;
+            front = node;
+            middle = node;
+            cnt++;
+            return;
+        }
+        if(!(cnt % 2)) middle = middle -> next;
+        LLNode* prevNode = middle -> prev;
+        if(prevNode) prevNode -> next = node;
+        node -> prev = prevNode;
+        node -> next = middle;
+        middle -> prev = node;
+        if(middle == front) front = node;
+        middle = node;
+        cnt++;
         return;
     }
     void pushBack(int val) {
-        if(!sizeDiff()) {
-            if(back.empty()) {
-                pushFront(val);
-                return;
-            }
-            int first = back.front();
-            front.push_back(first);
-            back.pop_front();
+        LLNode* node = new LLNode(val);
+        node -> prev = back;
+        if(back) back -> next = node;
+        back = node;
+        if(!cnt) {
+            front = back;
+            middle = back;
         }
-        back.push_back(val);
+        else if(!(cnt % 2)) middle = middle -> next;
+        cnt++;
         return;
     }
     int popFront() {
-        if(front.empty()) return -1;
-        if(!sizeDiff()) {
-            int first = back.front();
-            front.push_back(first);
-            back.pop_front();
+        if(!cnt) return -1;
+        int data = front -> val;
+        if(cnt == 1) {
+            front = back = middle = NULL;
+            cnt--;
+            return data;
         }
-        int val = front.front();
-        front.pop_front();
-        return val;
+        front = front -> next;
+        if(front) front -> prev = NULL;
+        if(!(cnt % 2)) middle = middle -> next;
+        cnt--;
+        return data;
     }
     int popMiddle() {
-        if(front.empty()) return -1;
-        int val = front.back();
-        front.pop_back();
-        if(sizeDiff()) {
-            int first = back.front();
-            front.push_back(first);
-            back.pop_front();
+        if(!cnt) return -1;
+        int data = middle -> val;
+        if(cnt == 1) {
+            front = back = middle = NULL;
+            cnt--;
+            return data;
         }
-        return val;
+        LLNode* prevNode = middle -> prev;
+        LLNode* nextNode = middle -> next;
+        if(prevNode) prevNode -> next = nextNode;
+        if(nextNode) nextNode -> prev = prevNode;
+        if(middle == front) front = nextNode;
+        if(middle == back) back = prevNode;
+        if(cnt % 2) middle = prevNode;
+        else middle = nextNode;
+        cnt--;
+        return data;
     }
     int popBack() {
-        if(back.empty()) return popMiddle();
-        if(sizeDiff()) {
-            int last = front.back();
-            back.push_front(last);
-            front.pop_back();
+        if(!cnt) return -1;
+        int data = back -> val;
+        if(cnt == 1) {
+            front = back = middle = NULL;
+            cnt--;
+            return data;
         }
-        int val = back.back();
-        back.pop_back();
-        return val;
+        back = back -> prev;
+        if(back) back -> next = NULL;
+        if(cnt % 2) middle = middle -> prev;
+        cnt--;
+        return data;
     }
 };
